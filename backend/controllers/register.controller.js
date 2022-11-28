236c22b2c03a582cff08fb912152b48c602b697e
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2022-11-28 16:22:12 +0000

    got editing attendance working

--- backend/controllers/register.controller.js
+++ backend/controllers/register.controller.js
@@ -67,6 +67,7 @@ exports.editRegisterItem = async (req, res) => {
     const student = await studentController.extendsStudentFind(req, res);
     if (!student) { return err => errors.error404(err, res) }
     else {
+        const studentlist = await classController.searchStudents(req, res);
         const objectid = studentlist.register.attendanceList.find(element => student);
         registerItemController.updateRegisterItem(objectid, req, res);
 
