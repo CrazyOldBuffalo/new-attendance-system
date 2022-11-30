e393a8c2cc8de9be16497697de9a4fbf096ddbb2
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2022-11-30 22:21:46 +0000

    updated to populate db

--- backend/app.js
+++ backend/app.js
@@ -55,7 +55,15 @@ async function createUsers() {
     "canEditCourse": false
   });
   const user5 = new User({
-    "username": "T",
+    "username": "Chris",
+    "password": "password",
+    "email": "test",
+    "telephone": "0123356",
+    "canEditModule": false,
+    "canEditCourse": false
+  });
+  const user6 = new User({
+    "username": "Ella",
     "password": "password",
     "email": "test",
     "telephone": "0123356",
@@ -68,6 +76,8 @@ async function createUsers() {
   user3.save(user3);
   user4.save(user4);
   user5.save(user5);
+  user6.save(user6);
+
   const student = new Student({
     studentID: "SU123",
     userRef: user4
@@ -76,8 +86,15 @@ async function createUsers() {
     studentID: "SU124",
     userRef: user5
   });
+  const student2 = new Student({
+    studentID: "SU125",
+    userRef: user6
+  });
+
   student.save(student);
   student1.save(student1);
+  student2.save(student2);
+
   const tutor1 = new Tutor({
     tutorID: "TU123",
     userRef: user1
@@ -87,49 +104,92 @@ async function createUsers() {
     userRef: user2
   });
 
+  const academicadvisor = new Advisor({
+    academicAdvisorID: "TU125",
+    userRef: user3,
+    students: [student, student1, student2]
+  });
+  academicadvisor.save(academicadvisor)
   tutor1.save(tutor1);
   tutor2.save(tutor2);
 
+  // register items for register 1.
   const regitem1 = new RegisterItem({
     students: student,
     attended: true
   });
   const regitem2 = new RegisterItem({
-    students: student1,
+    students: student,
     attended: true
   });
+
   regitem1.save(regitem1);
   regitem2.save(regitem2);
 
-  const academicadvisor = new Advisor({
-    tutorID: "TU132",
-    userRef: user3,
-    students: [student]
+  // register item for register 2.
+  const regitem3 = new RegisterItem({
+    students: student1,
+    attended: true
   });
-  academicadvisor.save(academicadvisor);
-  const register = new Register({
-    dateTime: Date.now(),
-    attendanceList: [regitem1, regitem2]
+  const regitem4 = new RegisterItem({
+    students: student1,
+    attended: true
   });
 
+  regitem3.save(regitem3)
+  regitem4.save(regitem4)
+
+  // register item for register 3.
+  const regitem5 = new RegisterItem({
+    students: student2,
+    attended: true
+  });
+  const regitem6 = new RegisterItem({
+    students: student2,
+    attended: true
+  });
+
+  regitem5.save(regitem5)
+  regitem6.save(regitem6)
+
+  const register = new Register({
+    dateTime: Date.now(),
+    attendanceList: [regitem1, regitem2, regitem3]
+  });
+
+  const register1 = new Register({
+    dateTime: Date.now(),
+    attendanceList: [regitem4, regitem5, regitem6]
+  })
+
   register.save(register);
+  register1.save(register1);
 
   const class1 = new Class({
     classID: "CL123",
     className: "Security",
-    students: [student],
+    students: [student, student1, student2],
     tutorRef: tutor1,
     register: register
   });
 
+  const class2 = new Class({
+    classID: "CL124",
+    className: "Software Architecture",
+    students: [student, student1, student2],
+    tutorRef: tutor2,
+    register: register1
+  });
+
   class1.save(class1);
+  class2.save(class2);
 
   const module = new Module({
-    moduleName: "SAD :(",
+    moduleName: "Software Architecture & Design",
     moduleID: "MD123",
-    students: student,
+    students: [student, student1, student2],
     moduleLeader: tutor1,
-    classes: [class1],
+    classes: [class1, class2],
     tutors: [tutor1, tutor2]
   });
 
@@ -138,8 +198,8 @@ async function createUsers() {
   const course = new Course({
     courseName: "Software Engineering bENG",
     courseID: "SE123",
-    students: [student],
-    courseLeader: tutor2,
+    students: [student, student1, student2],
+    courseLeader: tutor1,
     modules: [module]
   });
 
@@ -148,15 +208,15 @@ async function createUsers() {
 
 //Connects to the db and confirms connection
 db.mongoose.connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 }).then(() => {
-    console.log("Connected to DB");
+  console.log("Connected to DB");
 
-    //createUsers(); // populate db.
+  createUsers(); // populate db.
 }).catch(err => {
-    console.log("error connecting to db");
-    process.exit();
+  console.log("error connecting to db");
+  process.exit();
 });
 
 //sets the origin of CORS
@@ -173,7 +233,7 @@ require("./routes/course.routes")(app);
 require("./routes/module.routes")(app);
 require("./routes/class.routes")(app);
 require("./routes/register.routes")(app);
-require('./routes/auth.routes')(app); 
+require('./routes/auth.routes')(app);
 require('./routes/security.routes')(app);
 require('./routes/registeritem.routes')(app);
 
